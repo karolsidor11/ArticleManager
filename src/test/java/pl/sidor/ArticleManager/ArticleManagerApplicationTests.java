c7a82117b0eac1d0963531c18dbf32e6d61a87fd
KursJava <kursjava1@wp.pl>
2020-05-31 12:48:30 +0200

    Git init

---
+++ src/test/java/pl/sidor/ArticleManager/ArticleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sidor.ArticleManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
